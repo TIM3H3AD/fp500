a390e442c7e934923df7a17a7799c0ccaabef4cf
ethascloy <ethanclay@gmail.com>
2016-04-04 15:03:00 -0400

    init

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,2 +1,15 @@
 class Thing < ActiveRecord::Base
-end
+  # Associations
+
+  belongs_to :creator, class_name: User
+
+  ###################################
+
+  validates :title, presence: true
+  validates :description, presence: true, length: {maximum: 255}
+  validates :location, presence: true, length: {maximum: 30}
+  validates :price, presence: true
+
+  default_scope -> {order('title desc')}
+
+  end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+
+  # Associations
+
+  has_many :created_things, class_name: Thing, foreign_key: :creator_id, :dependent => :destroy
+ 
+###########################################################
+
+  before_save {self.email.downcase!}
+  validates :name, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, :format => {with: VALID_EMAIL_REGEX}, length: {maximum: 255}, uniqueness: true, if: :email_changed?
+  validates :password, presence: true, length: {minimum: 8}, allow_blank: true
+  has_secure_password
 end
